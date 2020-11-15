9de5bbb5e71d821911e51dec72c7233992ad27d9
Jerome Robbins <jerome@tregz.com>
2020-11-15 10:26:27 -0500

    Missing files

---
+++ iOS/Controllers/Item/SoloItemController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UIKit;
+
+namespace Kudo.iOS
+{
+    public partial class SoloItemController : UIViewController
+    {
+        public ItemDetailViewModel ViewModel { get; set; }
+        public SoloItemController(IntPtr handle) : base(handle) { }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            Title = ViewModel.Title;
+            ItemNameLabel.Text = ViewModel.Item.Text;
+            ItemDescriptionLabel.Text = ViewModel.Item.Description;
+        }
+    }
+}
